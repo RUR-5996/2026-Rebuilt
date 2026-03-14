1cf9fa75e534c6c36501d378e5c95901eeee1415
Maxim Filip <maxim@network.cz>
2026-03-14 23:04:53 +0100

    Add controller bindings for shooting while moving

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -136,6 +136,8 @@ public class RobotContainer {
     m_secondController.b().toggleOnTrue(SHOOTER.setTarget("OUTPOST"));
     m_secondController.leftBumper().toggleOnTrue(SHOOTER.aimOn());
     m_secondController.rightBumper().toggleOnTrue(SHOOTER.aimOff());
+    m_secondController.leftTrigger().onTrue(SHOOTER.shootWhileMovingOn());
+    m_secondController.rightTrigger().onTrue(SHOOTER.shootWhileMovingOff());
 
     /*
     m_secondController.pov(0).toggleOnTrue(SHOOTER.adjustShooterSpeed(0.02));
