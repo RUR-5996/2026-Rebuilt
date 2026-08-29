bd5bb3dbf1ae5ce7bb61a775c12aef77e757cd06
Maxim Filip <maxim@network.cz>
2026-08-29 13:46:33 +0200

    Remove unused shooter imports

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -21,25 +21,14 @@ import frc.robot.Constants.ShooterConstants;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.DriverStation.Alliance;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.DriverStation.Alliance;
 
-import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
-import static edu.wpi.first.units.Units.RevolutionsPerSecond;
-
-import java.util.Optional;
 import java.util.function.BooleanSupplier;
 
-import com.ctre.phoenix6.configs.CANcoderConfiguration;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.controls.VelocityVoltage;
@@ -66,9 +55,6 @@ public class Shooter extends SubsystemBase{
     SparkMaxConfig turretConfig;
     SoftLimitConfig turretLimitConfig;
     RelativeEncoder turretEncoder;
-
-//    CANcoder turretCANcoder;
-//    CANcoderConfiguration turretCANcoderConfig;
 
     private final VelocityVoltage feederVelocityVoltage = new VelocityVoltage(0);
 
@@ -251,10 +237,6 @@ public class Shooter extends SubsystemBase{
 
 
   public void calcTurretAbsRotation() {
-
-    ChassisSpeeds speeds = SWERVE.getChassisSpeeds();
-    double turretAbsVelX = speeds.vxMetersPerSecond; 
-    double turretAbsVelY = speeds.vyMetersPerSecond;
     
     double deltaX = x - turretXabs;
     double deltaY = y - turretYabs;
